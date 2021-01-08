26771342c5e84542bd9c6700901e4e3ba73b74ca
Islam Hassan <islamhassan4444@gmail.com>
2021-01-08 12:03:04 +0200

    added date

--- script.js
+++ script.js
@@ -233,6 +233,17 @@ btnSort.addEventListener('click', function (e) {
   sorted = !sorted;
 });
 
+///////////////////
+//////
+// set date
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+const month = `${now.getMonth()+1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = now.getHours();
+const min = now.getMinutes();
+labelDate.textContent = `${day}/${month}/${year},${hour}:${min}`;
+
 
 
 
